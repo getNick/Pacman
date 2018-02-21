c1c0ad0655b99f2608bdb2ef349b9b625d29c76c
getNick <aleksandr.kostiuk687@gmail.com>
2018-02-21 18:34:44 +0200

    crate startPage

--- Pacman/App.xaml.cs
+++ Pacman/App.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Autofac;
-using Pacman.Models;
-using Pacman.ViewModel;
+using WpfApplication.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -9,38 +8,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Pacman
+namespace WpfApplication
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            // Create your builder.
-            var builder = new ContainerBuilder();
-
-            // Usually you're only interested in exposing the type
-            // via its interface:
-            builder.RegisterInstance(new Maze()).As<IMaze>().SingleInstance();
-
-
-            // Scan an assembly for components
-            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
-            .Where(t => t.Name.EndsWith("Repository"))
-            .AsImplementedInterfaces()
-            .InstancePerRequest();
-
-            builder.RegisterType<MainWindowViewModel>().AsSelf();
-
-            var container = builder.Build();
-            
-            var model = container.Resolve<MainWindowViewModel>();
-            var view = new MainWindow { DataContext = model };
-            view.Show();
-
-           
-        }
     }
 }
---
+++ Pacman/ViewModel/StartPageViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using WpfApplication.Utils;
+using System.Windows.Input;
+
+namespace WpfApplication.ViewModel
+{
+    class StartPageViewModel:ViewModelBase
+    {
+        private const string NonRegister= "к сожелению,\n мы еще не знакомы";
+        public bool NewPlayer { get; set; } = true;
+        public string PlayerName { get; set; }
+        public StartPageViewModel()
+        {
+           var confName= ConfigurationManager.AppSettings["PlayerName"];
+            if (confName.Length > 1)
+            {
+                PlayerName = confName;
+                NewPlayer = false;
+            }
+            else
+            {
+                PlayerName = NonRegister;
+            }
+        }
+
+        #region SetName
+        RelayCommand _setName;
+        public ICommand SetNameCommand
+        {
+            get
+            {
+                if (_setName == null)
+                    _setName = new RelayCommand(SetName);
+                return _setName;
+            }
+        }
+        private void SetName(object parameter)
+        {
+            if (PlayerName.Length < 5)
+            {
+                return;
+            }
+            NewPlayer = false;
+            OnPropertyChanged("PlayerName");
+            OnPropertyChanged("NewPlayer");
+        }
+
+        #endregion
+        #region ResetName
+        RelayCommand _resetName;
+        public ICommand ResetNameCommand
+        {
+            get
+            {
+                if (_resetName == null)
+                    _resetName = new RelayCommand(ResetName);
+                return _resetName;
+            }
+        }
+        private void ResetName(object parameter)
+        {
+            PlayerName = NonRegister;
+            NewPlayer = true;
+            OnPropertyChanged("PlayerName");
+            OnPropertyChanged("NewPlayer");
+        }
+        #endregion
+
+        #region StartGame
+        RelayCommand _startGame;
+        public ICommand StartGameCommand
+        {
+            get
+            {
+                if (_startGame == null)
+                    _startGame = new RelayCommand(ResetName);
+                return _startGame;
+            }
+        }
+        private void StartGame(object parameter)
+        {
+
+        }
+        #endregion
+    }
+}
